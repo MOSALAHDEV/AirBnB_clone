14e3f0f6f877f208820bbea36bb8f7c86f794e3a
MOSALAHDEV <mosalah525446@gmail.com>
2025-01-25 12:58:41 -0800

    Modified tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from uuid import uuid4
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """ Test creation of BaseModel class """
     def test_base_model(self):
@@ -12,13 +13,14 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(type(bm.created_at), datetime)
         self.assertEqual(type(bm.updated_at), datetime)
         self.assertEqual(type(bm.to_dict()), dict)
+
     def test_save(self):
         """ Test save method """
         bm = BaseModel()
         bm.save()
         self.assertNotEqual(bm.created_at, bm.updated_at)
         self.assertEqual(type(bm.created_at), datetime)
-    
+
     def test_to_dict(self):
         """ Test to_dict method """
         bm = BaseModel()
@@ -27,7 +29,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(type(bm_dict['created_at']), str)
         self.assertEqual(type(bm_dict['updated_at']), str)
         self.assertEqual(type(bm_dict), dict)
-    
+
     def test_init_args_kwargs(self):
         """ Test init method with args and kwargs """
         bm = BaseModel()
@@ -42,6 +44,10 @@ class TestBaseModel(unittest.TestCase):
             """ Test __str__ method """
             bm = BaseModel()
             bm_str = str(bm)
-            self.assertEqual(bm_str, f"[{bm.__class__.__name__}] ({bm.id}) {bm.__dict__}")
+            self.assertEqual(
+                    bm_str,
+                    f"[{bm.__class__.__name__}] ({bm.id}) {bm.__dict__}"
+                )
+
     if __name__ == '__main__':
         unittest.main()
