e71c7ec32957d3b93d9b3839fb3d95a04952b3cb
MOSALAHDEV <mosalah525446@gmail.com>
2025-01-25 14:08:05 -0800

    added

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,13 @@
 import unittest
 from datetime import datetime
-from uuid import uuid4
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """ Test creation of BaseModel class """
+    """Test cases for the BaseModel class."""
+
     def test_base_model(self):
-        """ Test creation of BaseModel class """
+        """Test creation of BaseModel instance."""
         bm = BaseModel()
         self.assertIsInstance(bm, BaseModel)
         self.assertEqual(type(bm.created_at), datetime)
@@ -15,14 +15,14 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(type(bm.to_dict()), dict)
 
     def test_save(self):
-        """ Test save method """
+        """Test save method."""
         bm = BaseModel()
         bm.save()
         self.assertNotEqual(bm.created_at, bm.updated_at)
-        self.assertEqual(type(bm.created_at), datetime)
+        self.assertEqual(type(bm.updated_at), datetime)
 
     def test_to_dict(self):
-        """ Test to_dict method """
+        """Test to_dict method."""
         bm = BaseModel()
         bm_dict = bm.to_dict()
         self.assertEqual(bm_dict['__class__'], 'BaseModel')
@@ -31,7 +31,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(type(bm_dict), dict)
 
     def test_init_args_kwargs(self):
-        """ Test init method with args and kwargs """
+        """Test init method with args and kwargs."""
         bm = BaseModel()
         bm_dict = bm.to_dict()
         bm2 = BaseModel(**bm_dict)
@@ -41,21 +41,22 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(bm2.to_dict(), bm_dict)
 
     def test_str(self):
-        """ Test __str__ method """
+        """Test __str__ method."""
         bm = BaseModel()
         bm_str = str(bm)
         self.assertEqual(
-                bm_str,
-                f"[{bm.__class__.__name__}] ({bm.id}) {bm.__dict__}"
-            )
+            bm_str,
+            f"[{bm.__class__.__name__}] ({bm.id}) {bm.__dict__}"
+        )
 
     def test_without_args_kwargs(self):
-        """ Test init method without args and kwargs """
+        """Test init method without args and kwargs."""
         try:
             bm = BaseModel()
             self.assertIsInstance(bm, BaseModel)
         except Exception as e:
             self.fail(f"Exception raised: {e}")
 
-    if __name__ == '__main__':
-        unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
