aaa416fad117a8e85cbd913ea1f55c5435f1da33
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-01 02:10:50 -0800

    Fix: fixed tests for models

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,12 +5,11 @@ from models.city import City
 from models.base_model import BaseModel
 
 
-class TestBaseModel(unittest.TestCase):
+class Test_city(unittest.TestCase):
     """Test cases for city class"""
     def test_class(self):
         city = City()
         self.assertEqual(city.__class__, City)
-        self.assertEqual(city.__base__, BaseModel)
         self.assertEqual(city.state_id, "")
         self.assertEqual(city.name, "")
 
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,7 +2,7 @@
 " This module contains unit tests for the State class"
 import unittest
 from models.state import State
-
+from models.base_model import BaseModel
 
 class TestState(unittest.TestCase):
     """ This class contains unit tests for the State class"""
@@ -10,8 +10,9 @@ class TestState(unittest.TestCase):
     def test_init(self):
         """ Test init method """
         state = State()
-        self.assertIsInstance(state.__class__, State)
-        self.assertIsInstance(state.name, str)
+        self.assertEqual(state.__class__, State)
+        self.assertIsInstance(state, BaseModel)
+        self.assertEqual(state.name, "")
 
 
 if __name__ == '__main__':
